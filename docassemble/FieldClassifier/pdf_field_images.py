3c4478df4feaa1c93893e3cd24f1e058bc4cd70b
Quinten Steenhuis <qsteenhuis@gmail.com>
2022-12-20 17:27:09 -0500

    Get rid of stuff that can't be pickled

--- docassemble/FieldClassifier/pdf_field_images.py
+++ docassemble/FieldClassifier/pdf_field_images.py
@@ -24,10 +24,9 @@ class PDFFieldViewer(DAObject):
     def init(self, *pargs, **kwargs):
         super().init(*pargs, **kwargs)
         self._process_fields()
-        self._create_field_images()
     
     def _process_fields(self):
-        self.pdf = Pdf.open(self.filename)
+        pdf = Pdf.open(self.filename)
         self.fields = [
             {
                 "var_name": str(field.T),
@@ -35,32 +34,28 @@ class PDFFieldViewer(DAObject):
                 "field": field,
             }
             for field
-            in self.pdf.Root.AcroForm.Fields
+            in pdf.Root.AcroForm.Fields
         ]
-        self.images = convert_from_path(self.filename, dpi=250)
-        self.pdf_images = [tempfile.NamedTemporaryFile() for i in range(len(self.images))]
-        for file_obj, img in zip(self.pdf_images, self.images):
+        images = convert_from_path(self.filename, dpi=250)
+        pdf_images = [tempfile.NamedTemporaryFile() for _ in range(len(images))]
+        for file_obj, img in zip(pdf_images, images):
             img.save(file_obj, "JPEG")
             file_obj.flush()
 
-    def _create_field_images(self):
         for field in self.fields:
             bbox = field["field"].Rect.as_list()
-            field_page = self.pdf.pages.index(field["field"].P)
-            opencv_bbox = formfyxer.img2pdf_coords(bbox, self.images[field_page].height)
+            field_page = pdf.pages.index(field["field"].P)
+            opencv_bbox = formfyxer.img2pdf_coords(bbox, images[field_page].height)
             expanded_bbox = expand_bbox(opencv_bbox)
-            img = cv2.imread(self.pdf_images[field_page].name)
+            img = cv2.imread(pdf_images[field_page].name)
             field["image"] = bytes(cv2.imencode('.png', 
-                img[1:10,10:20]
+                img[1:100,150:200]
                 # img[
                 #     expanded_bbox[0]:expanded_bbox[1], 
                 #     expanded_bbox[2]:expanded_bbox[3]
                 # ]
             )[1])
-            del field["field"]
-
-    def get_fields(self):
-        return self.fields
+            del field["field"]      
 
 if __name__ == "__main__":
     pdf_path = "/mnt/e/My Drive/Test Weaver Files/civil_docketing_statement_polished_repaired.pdf"
